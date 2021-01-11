2486ecf3d5cd78f84215a3df5357b6419472336e
Erin Replogle <erin@adhocteam.us>
2021-01-11 08:43:02 -0700

    documentation updates

--- docs/src/pages/components/Button/Button.example.tsx
+++ docs/src/pages/components/Button/Button.example.tsx
@@ -5,15 +5,39 @@ import { Button } from "@cmsgov/design-system";
 const ButtonExample: FunctionComponent = () => {
   return (
     <div className="button-example">
-      <span className="ds-u-margin-right--1">
-        <Button className="ds-c-button--primary">Primary</Button>
-      </span>
-      <span className="ds-u-margin-right--1">
-        <Button className="ds-c-button--primary-alt">Secondary</Button>
-      </span>
-      <span className="ds-u-margin-right--1">
-        <Button>Tertiary</Button>
-      </span>
+      <h3 className="preview__label">Default</h3>
+      <div className="ds-u-padding--1">
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary">Primary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary-alt">Secondary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button>Tertiary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--transparent">Transparent</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button size="big">Landing Page</Button>
+        </span>
+      </div>
+      <h3 className="preview__label">Inverse</h3>
+      <div className="ds-u-fill--primary ds-u-padding--1 ds-u-margin-top--2">
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary ds-c-button--inverse">Primary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--inverse">Tertiary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--transparent ds-c-button--inverse">Transparent</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button size="big" className="ds-c-button--inverse">Landing Page</Button>
+        </span>
+      </div>
     </div>
   );
 };
