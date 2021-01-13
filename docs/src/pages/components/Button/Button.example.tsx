02a3a073e9b445dd8009da69a373c51c81bab2e3
Erin Replogle <erinrep@gmail.com>
2021-01-13 12:03:45 -0700

    MCT-4697: Button Redesign (#8)
    * MCT-4697: Button Redesign
    
    * swap inverse button example order
    
    * primary inverse active border fix
    
    * documentation updates
    
    * doc style fixes

--- docs/src/pages/components/Button/Button.example.tsx
+++ docs/src/pages/components/Button/Button.example.tsx
@@ -5,201 +5,39 @@ import { Button } from "@cmsgov/design-system";
 const ButtonExample: FunctionComponent = () => {
   return (
     <div className="button-example">
-      <table cellSpacing={0}>
-        <thead>
-          <tr>
-            <th colSpan={4}>Primary AKA Big buttons</th>
-          </tr>
-        </thead>
-        <tr>
-          <th></th>
-          <th>Sketch Name</th>
-          <th>Design System Name</th>
-          <th>Attributes</th>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary" size="big">
-              Button
-            </Button>
-          </td>
-          <td>Primary Teal</td>
-          <td>Primary Button, Big Size</td>
-          <td>
-            <code>
-              className=&quot;ds-c-button--primary&quot; size=&quot;big&quot;
-            </code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary-alt" size="big">
-              Button
-            </Button>
-          </td>
-          <td>Primary Blue</td>
-          <td>Primary Alt Button, Big Size</td>
-          <td>
-            <code>
-              className=&quot;ds-c-button--primary-alt&quot;
-              size=&quot;big&quot;
-            </code>
-          </td>
-        </tr>
-        <tr className="ds-base--inverse">
-          <td>
-            <Button size="big">Button</Button>
-          </td>
-          <td>Primary White</td>
-          <td>Default Button, Big Size</td>
-          <td>
-            <code>size=&quot;big&quot;</code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button size="big" disabled>
-              Button
-            </Button>
-          </td>
-          <td>Disabled</td>
-          <td>Any disabled big button</td>
-          <td>
-            <code>size=&quot;big&quot; disabled=true</code>
-          </td>
-        </tr>
-      </table>
-
-      <table cellSpacing={0}>
-        <thead>
-          <tr>
-            <th colSpan={4}>Secondary Buttons</th>
-          </tr>
-        </thead>
-        <tr>
-          <th></th>
-          <th>Sketch Name</th>
-          <th>Design System Name</th>
-          <th>Attributes</th>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary">Button</Button>
-          </td>
-          <td>Secondary Teal</td>
-          <td>Primary Button</td>
-          <td>
-            <code>className=&quot;ds-c-button--primary&quot;</code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary-alt">Button</Button>
-          </td>
-          <td>Secondary Blue</td>
-          <td>Primary Alt Button</td>
-          <td>
-            <code>className=&quot;ds-c-button--primary-alt&quot;</code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary-outline">Button</Button>
-          </td>
-          <td>Secondary Teal outline</td>
-          <td>Primary Outline Button</td>
-          <td>
-            <code>className=&quot;ds-c-button--primary-outline&quot;</code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary-alt-outline">Button</Button>
-          </td>
-          <td>Secondary Blue outline</td>
-          <td>Primary Alt Outline Button</td>
-          <td>
-            <code>className=&quot;ds-c-button--primary-alt-outline&quot;</code>
-          </td>
-        </tr>
-        <tr className="ds-base--inverse">
-          <td>
-            <Button>Button</Button>
-          </td>
-          <td>Secondary White</td>
-          <td>Default Button</td>
-          <td>none</td>
-        </tr>
-        <tr>
-          <td>
-            <Button disabled>Button</Button>
-          </td>
-          <td>Disabled</td>
-          <td>Any disabled button</td>
-          <td>
-            <code>disabled=true</code>
-          </td>
-        </tr>
-      </table>
-
-      <table cellSpacing={0}>
-        <thead>
-          <tr>
-            <th colSpan={4}>Link Buttons</th>
-          </tr>
-        </thead>
-        <tr>
-          <th></th>
-          <th>Sketch Name</th>
-          <th>Design System Name</th>
-          <th>Attributes</th>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary ds-c-button--transparent">
-              Button
-            </Button>
-          </td>
-          <td>Teal link</td>
-          <td>Primary Transparent Button</td>
-          <td>
-            <code>
-              className=&quot;ds-c-button--primary
-              ds-c-button--transparent&quot;
-            </code>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Button className="ds-c-button--primary-alt ds-c-button--transparent">
-              Button
-            </Button>
-          </td>
-          <td>Blue link</td>
-          <td>Primary Alt Transparent Button</td>
-          <td>
-            <code>
-              className=&quot;ds-c-button--primary-alt
-              ds-c-button--transparent&quot;
-            </code>
-          </td>
-        </tr>
-        <tr className="ds-base--inverse">
-          <td>
-            <Button className="ds-c-button--inverse ds-c-button--transparent">
-              Button
-            </Button>
-          </td>
-          <td>Link on dark background</td>
-          <td>Inverse Transparent Button</td>
-          <td>
-            <code>
-              className=&quot;ds-c-button--inverse
-              ds-c-button--transparent&quot;
-            </code>
-          </td>
-        </tr>
-      </table>
+      <h3 className="preview__label">Default</h3>
+      <div className="ds-u-padding--1">
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary">Primary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary-alt">Secondary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button>Tertiary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--transparent">Transparent</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button size="big">Landing Page</Button>
+        </span>
+      </div>
+      <h3 className="preview__label">Inverse</h3>
+      <div className="ds-u-fill--primary ds-u-padding--1 ds-u-margin-top--2">
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--primary ds-c-button--inverse">Primary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--inverse">Tertiary</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button className="ds-c-button--transparent ds-c-button--inverse">Transparent</Button>
+        </span>
+        <span className="ds-u-margin-right--1">
+          <Button size="big" className="ds-c-button--primary ds-c-button--inverse">Landing Page</Button>
+        </span>
+      </div>
     </div>
   );
 };
