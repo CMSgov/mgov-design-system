c40b3a39dcb69f3733356fe3a8f4091baf392284
Barb Miles <barbara@adhocteam.us>
2021-10-15 10:59:02 -0600

    Cleaned up the tooltip example to remove some of the extra divs and classes

--- docs/src/pages/components/Tooltip/Tooltip.example.jsx
+++ docs/src/pages/components/Tooltip/Tooltip.example.jsx
@@ -4,32 +4,27 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
-    <div className="ds-u-display--flex ds-u-align-items--center ds-u-margin-y--2">
-      <div className="ds-u-margin--0">
-        {'Tooltip with '}
-        <Tooltip
-          className="ds-c-tooltip__trigger-link"
-          component="a"
-          title="Tooltip trigger uses <a> for the trigger, styled with dotted underline"
-        >
-          inline trigger
-        </Tooltip>
-      </div>
+    <div className="example--wrapper ds-u-margin-top--6">
+      {'Tooltip with '}
+      <Tooltip
+        className="ds-c-tooltip__trigger-link"
+        placement="bottom"
+        component="a"
+        title="Tooltip trigger uses <a> for the trigger, styled with dashed underline"
+      >
+        inline trigger
+      </Tooltip>
     </div>
-    <div className="example--inverse example--wrapper">
-      <div className="">
-        <div className="ds-u-margin--0">
-          {'Inverse tooltip with '}
-          <Tooltip
-            className="ds-c-tooltip__trigger-link"
-            component="a"
-            title="Tooltip trigger uses <a> for the trigger, styled with dotted underline"
-            inversed
-          >
-            inline trigger
-          </Tooltip>
-        </div>
-      </div>
+    <div className="example--inverse example--wrapper ds-u-margin-bottom--2">
+      {'Inverse tooltip with '}
+      <Tooltip
+        className="ds-c-tooltip__trigger-link"
+        component="a"
+        title="Tooltip trigger uses <a> for the trigger, styled with dashed underline"
+        inversed
+      >
+        inline trigger
+      </Tooltip>
     </div>
   </>,
   document.getElementById('js-example')
